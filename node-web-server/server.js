7221efda344ef283da609ea4096245d9e2956b1a
Nick O'Ferrall <nickoferrall@gmail.com>
2018-12-20 15:38:05 -0500

    Hello Express complete

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,3 +1,23 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  //   res.send("<h1>Hello express</h1>");
+  res.send({
+    name: "Dave",
+    likes: ["football", "travel"]
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Didn't work old sport!"
+  });
+});
+
+app.listen(3000);
