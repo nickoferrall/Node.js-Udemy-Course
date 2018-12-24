b715a0eb2bc8f2019b50c8d45c015d433b36c11c
Nick O'Ferrall <nickoferrall@gmail.com>
2018-12-24 13:26:44 +0000

    Completed web servers and app application section

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,56 @@
 const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
 
-var app = express();
+const port = process.env.PORT || 3000;
+const app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append server.log.");
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs");
+// });
 
 app.use(express.static(__dirname + "/public"));
 
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
 app.get("/", (req, res) => {
-  //   res.send("<h1>Hello express</h1>");
-  res.send({
-    name: "Dave",
-    likes: ["football", "travel"]
+  res.render("home.hbs", {
+    pageTitle: "Home page",
+    welcomeMessage: "Heyyyyyyy"
   });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects"
+  });
 });
 
 app.get("/bad", (req, res) => {
@@ -22,4 +59,6 @@ app.get("/bad", (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
